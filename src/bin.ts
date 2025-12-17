e7536b08571d746c1e9363a688b93d780c6cc0fb
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-12-17 22:10:56 +0100

    f

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,5 @@
 import { console, textref, texts } from "@hazae41/stdbob"
 
-export function main(name: textref): i64 {
-  console.log(texts.from("Hello, " + texts.into(name) + "!"))
-
-  return 42
+export function sayMyName(name: textref): void {
+  console.log(texts.fromString("Hello, " + texts.toString(name) + "!"))
 }
