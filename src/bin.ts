497bf748c1f6a97cf57607729bc3ccbbd3db2462
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-12-18 23:20:05 +0100

    f

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,6 @@
-import { console, packref, storage, textref, texts } from "@hazae41/stdbob"
+import { console, storage, textref, texts } from "@hazae41/stdbob"
 
-export function sayMyName(name: textref): packref {
+export function sayMyName(name: textref): textref {
   const previous = storage.get<textref>(texts.fromString("name"))
 
   console.log(texts.fromString("Hello, " + texts.toString(name) + "!"))
